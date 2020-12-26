927c10dd5704f1955f9b3f8b38bd0f3726a091a0
RamanSB <ramansb100@gmail.com>
2020-12-26 17:38:01 +0000

    Solution for Occupations challenge, however variable declarations are not parsed correctly by hacker rank interpreter/compiler

--- sql/src/Occupations.sql
+++ sql/src/Occupations.sql
@@ -20,31 +20,30 @@
 
 SELECT * FROM OCCUPATIONS;
 
-SELECT
-	(CASE
-		WHEN Occupation = 'Doctor' THEN Name
-	END) As Doctor,
-    (CASE
-		WHEN Occupation = 'Professor' THEN Name
-	END) As Actor,
-    (CASE
-		WHEN Occupation = 'Singer' THEN Name
-	END) AS Singer,
-    (CASE
-		WHEN Occupation = 'Actor' THEN Name
-	END) AS Actor
-FROM OCCUPATIONS
-ORDER BY Name;
-
---SQL currently returns the below
-
---NULL,'Ashely',NULL,NULL
---NULL,'Christeen',NULL,NULL
---NULL,NULL,NULL,'Jane'
---'Jenny',NULL,NULL,NULL
---NULL,NULL,NULL,'Julia'
---NULL,'Ketty',NULL,NULL
---NULL,NULL,NULL,'Maria'
---NULL,NULL,'Meera',NULL
---NULL,NULL,'Priya',NULL
---'Samantha',NULL,NULL,NULL
+-- Declaring variables
+SET @d=0, @p=0, @s=0, @a=0;
+SELECT MAX(intermediate.Doctor), MAX(intermediate.Professor), MAX(intermediate.Singer), MAX(intermediate.Actor) FROM
+	(SELECT
+		(CASE
+			WHEN Occupation = 'Doctor' THEN (@d:=@d+1)
+			WHEN Occupation = 'Professor' THEN (@p:=@p+1)
+			WHEN Occupation = 'Singer' THEN (@s:=@s+1)
+			WHEN Occupation = 'Actor' THEN (@a:=@a+1)
+		END) as Count,
+		(CASE
+			WHEN Occupation = 'Doctor' THEN Name
+		END) As Doctor,
+		(CASE
+			WHEN Occupation = 'Professor' THEN Name
+		END) As Professor,
+		(CASE
+			WHEN Occupation = 'Singer' THEN Name
+		END) AS Singer,
+		(CASE
+			WHEN Occupation = 'Actor' THEN Name
+		END) AS Actor
+	FROM OCCUPATIONS
+	ORDER BY Name) intermediate
+GROUP BY intermediate.count -- This ensures that where a value exists with column value set to 1, all corresponding names will be on the same row - the same applies for rows 2 & 3
+-- however when invoking GROUP BY we must ensure we use an aggregate function
+-- https://www.w3schools.com/sql/sql_groupby.asp#:~:text=The%20GROUP%20BY%20statement%20groups,by%20one%20or%20more%20columns.
