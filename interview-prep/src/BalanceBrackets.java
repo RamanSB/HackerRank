a8de6100ea3c0dd730d26753f8557c48634c7e12
RamanSB <ramansb100@gmail.com>
2021-01-07 05:04:25 +0000

    Attempt at BalancedBracket problem, test case 1/21 failed - lazy exception approach

---
+++ interview-prep/src/BalanceBrackets.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+/**
+ * Problem: https://www.hackerrank.com/challenges/balanced-brackets/problem
+ *
+ * The following chars are considered as 'brackets': (, ), {, }, [, or ].
+ * A bracket is matched if the following occurs: [], {}, and ().
+ *
+ * A string is balanced if:
+ * - It contains no unmatched brackets.
+ * - The subset of brackets enclosed within the confines of a matched pair of brackets is also a matched pair of brackets.
+ */
+public class BalanceBrackets {
+
+    // Complete the isBalanced function below.
+    static String isBalanced(String s) {
+
+        List<Character> openBracketList = Arrays.asList('{', '(', '[');
+        List<String> matchedBracketList = Arrays.asList("{}","[]","()");
+
+        Deque<Character> stack = new ArrayDeque<>();
+        boolean isBalanced = false;
+        for(int i=0; i<s.length(); i++){
+            try {
+                char character = s.charAt(i);
+                if (openBracketList.contains(character)) {
+                    stack.push(character);
+                } else {
+                    isBalanced = matchedBracketList.contains(stack.pop().toString() + s.charAt(i));
+                    if (!isBalanced) {
+                        break; // if the brackets are unbalanced for any iteration, return false;
+                    }
+                }
+            } catch(Exception ex){
+                isBalanced = false;
+                break;
+            }
+        }
+        return isBalanced ? "YES" : "NO";
+    }
+
+    public static void main(String[] args){
+        String input = "}][}}(}][))]";
+        System.out.println(isBalanced(input));
+    }
+
+}
