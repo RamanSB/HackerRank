d83bdb94bfea28a6522a9592f1b891135e2834f5
RamanSB <ramansb100@gmail.com>
2021-01-13 03:54:55 +0000

    Problem solved for: https://www.hackerrank.com/challenges/java-exception-handling-try-catch/problem

---
+++ Java/src/ExceptionHandlingProblem.java
@@ -0,0 +1,24 @@
+/**
+ * Problem: https://www.hackerrank.com/challenges/java-exception-handling-try-catch/problem
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class ExceptionHandlingProblem {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputArg1 = scanner.nextLine();
+        String inputArg2 = scanner.nextLine();
+        try{
+            int x = Integer.parseInt(inputArg1);
+            int y = Integer.parseInt(inputArg2);
+            System.out.println(x/y);
+        } catch(NumberFormatException ex){
+            System.out.println(new InputMismatchException());
+        } catch(ArithmeticException ex){
+            System.out.println(ex);
+        }
+    }
+}
